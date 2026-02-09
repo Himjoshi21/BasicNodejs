932789026cb2698fccfc521248d8d5198299da66
Himjoshi21 <joshihimanshu9295@gmail.com>
2026-02-09 08:39:35 +0530

    feat: implement task creation and retrieval with authentication middleware

--- Day4-Expressjs/routers/task.routes.js
+++ Day4-Expressjs/routers/task.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { createTask } from "../controllers/task.controller.js";
-
+import { authMiddleware } from "../middlewares/auth.middleware.js";
+import { getAllTasks } from "../controllers/task.controller.js";
 const taskRoutes = Router();
 
-taskRoutes.get("/",createTask)
-
+taskRoutes.get("/",authMiddleware,createTask)
+taskRoutes.get("/",authMiddleware,getAllTasks)
 export default taskRoutes;
